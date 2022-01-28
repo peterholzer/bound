e98396e319993e3ebcbb25dd0d9f16a429bd1a25
Peter Holzer <peterholzer@users.noreply.github.com>
2022-01-28 20:52:16 +0100

    Update f.lua

--- f.lua
+++ f.lua
@@ -1,10 +1,13 @@
+
+
+-- Append multiple arguments to a table
 local function table_append(t, ...)
     for i = 1, select('#', ...) do
         table.insert(t, (select(i, ...))) -- extra braces needed
     end
 end
 
-
+-- Call a function once for each argument
 function call_each(f, ...)
     print('call_each', f, ...)
     local result = {}
@@ -14,18 +17,16 @@ function call_each(f, ...)
     return table.unpack(result)
 end
 
--- call_each(print, 'a', 'b', 'c', 'd')
-
-
-
+-- Return a function that calls f for each argument
+-- and calls g for each return value of f
+-- and returns a flat list
 function bind2(f, g)
     return function(...)
         return call_each(g, call_each(f, ...))
     end
 end
 
-
-
+-- Overload bitwise right shift operator for functions
 debug.setmetatable(function() end, {
     __shr = bind2,
 })
@@ -35,10 +36,8 @@ debug.setmetatable(function() end, {
 
 
 
-
-
 function a(x)
-    return '('..x..')'
+    return '('..x..'1'..')', '('..x..'2'..')'
 end
 
 function b(x)
@@ -63,9 +62,12 @@ local bound = a >> b >> c >> d >> print
 
 bound('l', 'm', 'n')
 
--- print   <{[(l)]}>
--- print   <{[(m)]}>
--- print   <{[(n)]}>
+-- print   <{[(l1)]}>
+-- print   <{[(l2)]}>
+-- print   <{[(m1)]}>
+-- print   <{[(m2)]}>
+-- print   <{[(n1)]}>
+-- print   <{[(n2)]}>
 
 
 
