e4bff4eeba83116814534022966d3482dddb6c97
Peter Holzer <peterholzer@users.noreply.github.com>
2022-01-28 20:28:55 +0100

    Update f.lua

--- f.lua
+++ f.lua
@@ -10,6 +10,31 @@ function call_each(f, ...)
     return table.unpack(result)
 end
 
-call_each(print, 'a', 'b', 'c', 'd')
+-- call_each(print, 'a', 'b', 'c', 'd')
+
+
+function bind2(f, g)
+    return function(...)
+        return g(f(...))
+    end
+end
+
+
+
+
+debug.setmetatable(function() end, {
+    __shr = bind2,
+    __shr = function(f, g)
+        return function(...)
+            return call_each(bind2(f, g), ...)
+        end
+    end,
+})
+
+
+
+local bound = a >> b >> c >> d >> print
+
+bound('l', 'm', 'n')
 
 
